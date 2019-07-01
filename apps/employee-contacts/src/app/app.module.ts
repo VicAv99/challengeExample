0bf2ade16aa0237633926abd44b9eee50643d08c
Victor Avila <victor.avila@venturplex.com>
2019-07-01 16:56:27 -0700

    first attempt to update

--- apps/employee-contacts/src/app/app.module.ts
+++ apps/employee-contacts/src/app/app.module.ts
@@ -1,11 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { UiToolbarModule } from '@workspace/ui-toolbar';
+import { UiLoginModule } from '@workspace/ui-login';
+import { MaterialModule } from '@workspace/material';
+import { CoreDataModule } from '@workspace/core-data';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    CoreDataModule,
+    MaterialModule,
+    UiLoginModule,
+    UiToolbarModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
