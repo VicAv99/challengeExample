f489de6966a5b759a405ef347c697d91855d82fb
Victor Avila <victor.avila@venturplex.com>
2019-07-01 18:57:47 -0700

    create: login

--- libs/ui-login/src/lib/login/login.component.ts
+++ libs/ui-login/src/lib/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'workspace-login',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.initForm();
+  }
+
+  displayError(control: string, errorType: string) {
+    return this.form.get(control).hasError(errorType);
+  }
+
+  logIn() {
+    if (this.form.valid) {
+      this.router.navigate(['employee-contacts']);
+    }
+  }
+
+  private initForm() {
+    this.form = this.formBuilder.group({
+      username: ['', Validators.compose([Validators.required])],
+      password: ['', Validators.compose([Validators.required])]
+    }, {updateOn: 'blur'});
   }
 
 }
--- libs/ui-login/src/lib/ui-login.module.ts
+++ libs/ui-login/src/lib/ui-login.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MaterialModule } from '@workspace/material';
+
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule
+  ],
   declarations: [LoginComponent],
   exports: [LoginComponent]
 })
