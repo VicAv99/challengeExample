89bc366e0d92455cd2d3f9f6192ff32998b353d5
Victor Avila <victor.avila@venturplex.com>
2019-07-01 20:52:21 -0700

    create: logout

--- libs/ui-toolbar/src/lib/toolbar/toolbar.component.ts
+++ libs/ui-toolbar/src/lib/toolbar/toolbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { MatSidenav } from '@angular/material';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'workspace-toolbar',
@@ -10,4 +11,10 @@ export class ToolbarComponent {
   @Input() title: string;
   @Input() sidenav: MatSidenav;
   @Input() isAuthenticated: boolean;
+
+  constructor(private router: Router) {}
+
+  logOut() {
+    this.router.navigate(['login']);
+  }
 }
